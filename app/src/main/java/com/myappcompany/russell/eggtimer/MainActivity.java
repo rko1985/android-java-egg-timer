92663f169a5b17417efa769f87cfa04c1d7a7699
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-13 09:02:19 -1000

    seek bar changes text view minutes/sec

--- app/src/main/java/com/myappcompany/russell/eggtimer/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/eggtimer/MainActivity.java
@@ -2,6 +2,8 @@ package com.myappcompany.russell.eggtimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar timerSeekBar = findViewById(R.id.timerSeekBar);
+        final TextView timerTextView = findViewById(R.id.countdownTextView);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int minutes = i / 60;
+                int seconds = i - (minutes * 60);
+
+                String secondString = Integer.toString(seconds);
+
+                if(secondString.equals("0")){
+                    secondString = "00";
+                }
+
+                timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
